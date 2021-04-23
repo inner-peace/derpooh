27cafc62eecc1e215af8b6b84d0329b12129c93a
peace <peace@yandex-team.ru>
2021-04-23 11:38:32 +0300

    did better

--- server/index.js
+++ server/index.js
@@ -2,29 +2,37 @@ const express = require('express')
 const app = express()
 const port = 3001
 
+// allow cors for local dev
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Cache-Control");
+    next();
+});
+
 app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({extended: true})) // for parsing application/x-www-form-urlencoded
+
 
 const users = {
     'demo@demo.com': {
         email: 'demo@demo.com',
-        token: '12312adasd'
+        token: '12312adasd',
+        password: 'demo'
     }
 }
 
 app.post('/api/validate', (req, res) => {
-    if (req.body.email) {
-        res.json({
-            status: true
-        })
-    }
+    res.json({
+        status: req.body.email in users
+    })
 })
 
 app.post('/api/login', (req, res) => {
-    if (req.body.email && users[req.body.email]) {
+    if (req.body.email && users[req.body.email] && users[req.body.email].password === req.body.password) {
         res.json({
             status: true,
-            data: users[req.body.email],
+            token: users[req.body.email].token,
         })
     } else {
         res.json({
